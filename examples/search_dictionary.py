c64b288e705e3670620bcf98f74383f8d38ce924
Jacob Nick <QWU4007@hca.corpad.net>
2026-01-13 12:54:24 -0600

    update example

--- examples/search_dictionary.py
+++ examples/search_dictionary.py
@@ -7,18 +7,23 @@ Searches for a list of columns in the dictionary
 dictionary = Dictionary("path/to/dictionary")
 columns_to_search = [
     "coid",
-    "iplan",
-    "discharge_date",
-    "net_revenue"
+    "eff_timestamp",
+    "street_address_1"
 ]
 
 for column in columns_to_search:
+
+    # we can do a direct lookup by column name
     lookup_result = dictionary.lookup(column)
 
     # if we didn't get any hits with the direct lookup, we can do a fuzzy lookup
     if len(lookup_result) == 0:
-        lookup_result = dictionary.fuzzy_lookup(column, threshold=90)
+        lookup_result = dictionary.fuzzy_lookup(column, threshold=75)
 
     # decide what to do with the lookup result
     # ex print all descriptions
-    print([(result.name, result.description) for result in lookup_result])
+    for result in lookup_result:
+        print(f"Lookup value: {column}")
+        print(f"Column name: {result.name}")
+        print(f"Description: {result.description}")
+        print("\n")
