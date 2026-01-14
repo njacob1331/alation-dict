f20967e987be86e24cdd24a5bb5b972a9f733a7f
Jacob Nick <QWU4007@hca.corpad.net>
2026-01-14 09:24:16 -0600

    add url field + validator

--- src/alation_dict/record.py
+++ src/alation_dict/record.py
@@ -1,3 +1,5 @@
+from typing import ClassVar
+from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 from pydantic import BaseModel, ConfigDict, field_validator
 
@@ -6,6 +8,7 @@ class Record(BaseModel):
     Type respresenting both the Alation API response object
     and corresponding dictionary entry.
     """
+    base_url: ClassVar[str] = "https://alation.medcity.net"
     model_config = ConfigDict(
         extra="allow",
         frozen=True
@@ -15,7 +18,12 @@ class Record(BaseModel):
     name: str
     title: str
     description: str
+    url: str
 
     @field_validator("description")
     def format_description(cls, v: str):
         return BeautifulSoup(v, "html.parser").get_text(strip=True)
+
+    @field_validator("url")
+    def format_url(cls, v: str):
+        return urljoin(cls.base_url, v)
