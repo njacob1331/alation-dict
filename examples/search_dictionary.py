77243ee189c06a7a4f0a3b3457cbcfc993e53974
Jacob Nick <QWU4007@hca.corpad.net>
2026-02-12 11:40:55 -0600

    update examples

--- examples/search_dictionary.py
+++ examples/search_dictionary.py
@@ -1,29 +1,39 @@
-from alation_dict import Dictionary
+from alation_dict import Dictionary, Storage, StorageType
 
 """
 Searches for a list of columns in the dictionary
 """
 
-dictionary = Dictionary("path/to/dictionary")
-columns_to_search = [
+# first initalize the dictionary
+storage = Storage(StorageType.FILE,"path/to/dictionary")
+dictionary = Dictionary(storage)
+
+# example of new columns that we want to lookup
+columns_to_lookup = [
     "coid",
     "eff_timestamp",
-    "street_address_1"
+    "street_address",
 ]
 
-for column in columns_to_search:
+for column in columns_to_lookup:
 
     # we can do a direct lookup by column name
     lookup_result = dictionary.lookup(column)
 
     # if we didn't get any hits with the direct lookup, we can do a fuzzy lookup
     if len(lookup_result) == 0:
-        lookup_result = dictionary.fuzzy_lookup(column, threshold=75)
+        lookup_result = dictionary.fuzzy_lookup(column, 75)
 
     # decide what to do with the lookup result
     # ex print all descriptions
+    # we could also export if we want: dictionary.export_records(lookup_result, "export.csv")
+    print(f"Query: {column}")
     for result in lookup_result:
-        print(f"Lookup value: {column}")
-        print(f"Column name: {result.name}")
-        print(f"Description: {result.description}")
-        print("\n")
+        print(f"""
+        Search Result
+        -------------
+        Column Name : {result.name}
+        Description : {result.description}
+        Source: {result.url}
+
+        """)
--- examples/update_dictionary.py
+++ examples/update_dictionary.py
@@ -1,15 +1,19 @@
-from alation_dict import Client, Dictionary
+from alation_dict import Client, Dictionary, Storage, StorageType
 
 """
 Updates the dictionary based on the records contained in the Alation API response.
 This script is intended to be ran as a scheduled job.
 """
 
-# api token info: https://developer.alation.com/dev/docs/authentication-into-alation-apis
-api_token = "<YOUR API TOKEN>"
+# auth info: https://developer.alation.com/dev/docs/authentication-into-alation-apis
+auth_token = "<YOUR API TOKEN>"
 
-dictionary = Dictionary(path="path/to/dictionary")
-client = Client(api_token, config_path="path/to/config")
+# api client for making requests to Alation
+client = Client(auth_token)
+# specifies where we want to read from and write to
+storage = Storage(StorageType.FILE, "path/to/dictionary")
+# builds a searchable dictionary from the raw records
+dictionary = Dictionary(storage)
 
 for record in client.api_response():
     dictionary.add(record)
