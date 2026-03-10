9661d0b7c87bbf7b7a9c51f65ab70bd0942abb25
njacob1331 <155578121+njacob1331@users.noreply.github.com>
2026-03-10 10:49:12 -0500

    Import Endpoint and add to __all__ exports

--- src/alation_dict/__init__.py
+++ src/alation_dict/__init__.py
@@ -1,10 +1,11 @@
-from .client import Client
+from .client import Client, Endpoint
 from .dictionary import Dictionary
 from .record import Record
 from .storage import Storage, StorageType
 
 __all__ = [
     "Client",
+    "Endpoint",
     "Dictionary",
     "Record",
     "Storage",
