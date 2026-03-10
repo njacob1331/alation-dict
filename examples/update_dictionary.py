71fac1ca7c8dbbbfa97f11286299100a88b3f475
njacob1331 <155578121+njacob1331@users.noreply.github.com>
2026-03-10 10:47:16 -0500

    update example

--- examples/update_dictionary.py
+++ examples/update_dictionary.py
@@ -1,21 +1,19 @@
-from alation_dict import Client, Dictionary, Storage, StorageType
+from alation_dict.dictionary import Dictionary
+from alation_dict.client import Client, Endpoint
+from alation_dict.storage import Storage, StorageType
 
 """
-Updates the dictionary based on the records contained in the Alation API response.
-This script is intended to be ran as a scheduled job.
+Builds/updates the dictionary based on the records contained in the Alation API response.
 """
 
 # auth info: https://developer.alation.com/dev/docs/authentication-into-alation-apis
-auth_token = "<YOUR API TOKEN>"
+auth_token = "<YOUR_API_TOKEN>"
 
-# api client for making requests to Alation
 client = Client(auth_token)
-# specifies where we want to read from and write to
 storage = Storage(StorageType.FILE, "path/to/dictionary")
-# builds a searchable dictionary from the raw records
 dictionary = Dictionary(storage)
 
-for record in client.api_response():
+for record in client.get(Endpoint.COLUMN):
     dictionary.add(record)
 
 dictionary.save()
