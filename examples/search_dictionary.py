d19506353bed38b965f180f4298b7aa8733f0da3
njacob1331 <155578121+njacob1331@users.noreply.github.com>
2026-03-19 09:23:55 -0500

    update example

--- examples/search_dictionary.py
+++ examples/search_dictionary.py
@@ -1,39 +1,26 @@
 from alation_dict import Dictionary, Storage, StorageType
 
 """
-Searches for a list of columns in the dictionary
+Search the dictionary
 """
 
-# first initalize the dictionary
-storage = Storage(StorageType.FILE,"path/to/dictionary")
+storage = Storage(StorageType.LOCAL_FILE, "path/to/dictionary")
 dictionary = Dictionary(storage)
 
-# example of new columns that we want to lookup
-columns_to_lookup = [
-    "coid",
-    "eff_timestamp",
-    "street_address",
-]
+lookup_result = dictionary.lookup("coid")
 
-for column in columns_to_lookup:
+# we can also do a fuzzy lookup if we don't get any results
+# from the direct lookup
+#
+# if len(lookup_result) == 0:
+#   lookup_result = dictionary.fuzzy_lookup("coid", threshold=90)
 
-    # we can do a direct lookup by column name
-    lookup_result = dictionary.lookup(column)
+for result in lookup_result:
+    print(f"""
+    Search Result
+    -------------
+    Column Name : {result.name}
+    Description : {result.description}
+    Source: {result.url}
 
-    # if we didn't get any hits with the direct lookup, we can do a fuzzy lookup
-    if len(lookup_result) == 0:
-        lookup_result = dictionary.fuzzy_lookup(column, 75)
-
-    # decide what to do with the lookup result
-    # ex print all descriptions
-    # we could also export if we want: dictionary.export_records(lookup_result, "export.csv")
-    print(f"Query: {column}")
-    for result in lookup_result:
-        print(f"""
-        Search Result
-        -------------
-        Column Name : {result.name}
-        Description : {result.description}
-        Source: {result.url}
-
-        """)
+    """)
